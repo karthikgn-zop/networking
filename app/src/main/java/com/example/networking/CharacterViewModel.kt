9a9bc59398901f2092c6f4943a871cdacc46d2fa
karthikgn-zop <karthik.gn@zopping.com>
2026-03-04 11:35:02 +0530

    Added scrollable and extra characters

--- app/src/main/java/com/example/networking/CharacterViewModel.kt
+++ app/src/main/java/com/example/networking/CharacterViewModel.kt
@@ -12,18 +12,25 @@ import kotlinx.coroutines.launch
 class CharacterViewModel : ViewModel() {
     private val _characters = mutableStateOf<List<Character>>(emptyList())
     val characters: State<List<Character>> = _characters
-
+    private  var currentPage=1
+    private  var isLoading=false
     init {
         fetchCharacters()
     }
 
-    private fun fetchCharacters() {
+    fun fetchCharacters() {
+        if(isLoading) return
+
         viewModelScope.launch {
             try {
-                val response = RetrofitInstance.api.getCharacters()
-                _characters.value = response.results
+                isLoading=true
+                val response = RetrofitInstance.api.getCharacters(currentPage)
+                _characters.value += response.results
+                currentPage++
+                isLoading=false
                 Log.d("API_TEST", response.toString())
             } catch (e: Exception) {
+                isLoading=false
                 e.printStackTrace()
                 Log.e("API_ERROR", e.message ?: "Unknown error")
             }
